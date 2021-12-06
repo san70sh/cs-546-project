5b81eaa8af9764cd80e4c333cd27f3b2df30c201
Yuyun Chen <qqjoyko@gmail.com>
2021-12-06 01:31:28 -0500

    user routes

--- Project_Code/routes/users.js
+++ Project_Code/routes/users.js
@@ -4,21 +4,21 @@ const router = express.Router();
 const users = require("../data/users");
 
 router.post("/login", async (req, res) => {
-    let username = req.body.username;
+    let email = req.body.email;
     let password = req.body.password;
-    if (typeof username !== 'string' || typeof password !== 'string') {
-        res.status(400).render("pages/loginform", {message:'username and passwork must be string', error: true});
+    if (typeof email !== 'string' || typeof password !== 'string') {
+        res.status(400).render("pages/loginform", {message:'email and passwork must be string', error: true});
         return;
     }
-    username = username.trim().toLowerCase();
+    email = email.trim().toLowerCase();
     password = password.trim();
-    if (username.length === 0 || password.length === 0) {
-        res.status(400).render("pages/loginform", {message:'username and passwork must be non empty string and can\'t just be space', error: true});
+    if (email.length === 0 || password.length === 0) {
+        res.status(400).render("pages/loginform", {message:'email and passwork must be non empty string and can\'t just be space', error: true});
         return;
     }
     let matchStr = /^[a-z0-9]{4,}$/i;
-    if (!matchStr.test(username)) {
-        res.status(400).render("pages/loginform", {message:'username can only be alphanumeric characters and should be at least 4 characters long.', error: true});
+    if (!matchStr.test(email)) {
+        res.status(400).render("pages/loginform", {message:'email can only be alphanumeric characters and should be at least 4 characters long.', error: true});
         return;
     }
     if (password.length < 6) {
@@ -31,29 +31,68 @@ router.post("/login", async (req, res) => {
     }
     let tmp;
     try {
-        tmp = await users.checkUser(username, password);
+        tmp = await users.checkUser(email, password);
     } catch (e) {
         res.status(400).render("pages/loginform", {message:e, error: true});
         return;
     }
     if (tmp.authenticated === true) {
-        req.session.user = username;//user name or id?
-        res.redirect("/");//goto main page if user has logined in
+        req.session.user = email;//user name or id?
+        res.redirect("/");//goto main page if user logined in
     } else {
         res.status(400).render("pages/loginform", {message:"please try again", error: true});
         return;
     }
 })
 
-
-router.get('/favor/:id', async (req, res) => {//get all favor 
-    let id = req.params.id;
+// if ...  should have else throw otherwise it would have no respondes
+router.get('/favor', async (req, res) => {//get all favor 
+    let userId = req.body.userId;
     try {
-        if(ObjectId.isValid(id)) {
-            let output = await recruiterDat.getRecruiter(id);
+        if(ObjectId.isValid(userId)) {
+            let output = await users.getFavourites(userId);
             return res.json(output);
         }
     } catch (e) {
-        return res.status(e.status).render('partials/rec', {title: "Invalid User", message: e.message, err: true});
+        return res.status(e.status).render('pages/error', {title: "Favor", message: e.message, error: true});
+    }
+});
+
+router.post('/favor', async (req, res) => {
+    let jobId = req.body.jobId;
+    let userId = req.body.userId;
+    try {
+        if(ObjectId.isValid(jobId) && ObjectId.isValid(userId)) {
+            let output = await users.Favorites(jobId, userId);
+            return res.json(output);
+        }
+    } catch (e) {
+        return res.status(e.status).render('pages/error', {title: "Apply", message: e.message, err: true});
+    }
+});
+
+router.delete('/favor', async (req, res) => {
+    let jobId = req.body.jobId;
+    let userId = req.body.userId;
+    try {
+        if(ObjectId.isValid(jobId) && ObjectId.isValid(userId)) {
+            let output = await users.delFavourites(jobId, userId);
+            return res.json(output);
+        }
+    } catch (e) {
+        return res.status(e.status).render('pages/error', {title: "Favor", message: e.message, err: true});
+    }
+});
+
+router.post('/apply', async (req, res) => {
+    let jobId = req.body.jobId;
+    let userId = req.body.userId;
+    try {
+        if(ObjectId.isValid(jobId) && ObjectId.isValid(userId)) {
+            let output = await users.apply(jobId, userId);
+            return res.json(output);
+        }
+    } catch (e) {
+        return res.status(e.status).render('pages/error', {title: "Apply", message: e.message, err: true});
     }
 });
