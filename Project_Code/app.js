82fe1f6aefe1bd10442c326ba9b2d59a6caa8470
You Wang <w919542445@gmail.com>
2021-12-07 01:13:16 -0500

    allow to do math on hbs {{@index}}

--- Project_Code/app.js
+++ Project_Code/app.js
@@ -1,42 +1,61 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 //const session = require('express-session');
-const static = express.static(__dirname + '/public');
-const configRoutes = require('./routes');
-const exphbs = require('express-handlebars');
-const session = require('express-session');
+const static = express.static(__dirname + "/public");
+const configRoutes = require("./routes");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
 
-app.use('/public', static);
+app.use("/public", static);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    name: 'AuthCookie',
+    name: "AuthCookie",
     secret: "This is a secret.. shhh don't tell anyone",
     saveUninitialized: true,
-    resave: false
+    resave: false,
   })
 );
 
-app.use('/users', (req, res, next) => {//*****************setting private pages
-  if (!req.session.userId) {
-    res.status(200).render("pages/error", {error: "you are not logged in"});
-    return;
-  } else {
-    req.body.userId = req.session.userId; // give userId to next routes
-    next();
-  }
-});
+// app.use('/users', (req, res, next) => {//*****************setting private pages
+//   if (!req.session.userId) {
+//     res.status(200).render("pages/error", {error: "you are not logged in"});
+//     return;
+//   } else {
+//     req.body.userId = req.session.userId; // give userId to next routes
+//     next();
+//   }
+// });
 
 app.use(express.json());
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+    helpers: {
+      // Function to do basic mathematical operation in handlebar
+      math: function (lvalue, operator, rvalue) {
+        lvalue = parseFloat(lvalue);
+        rvalue = parseFloat(rvalue);
+        return {
+          "+": lvalue + rvalue,
+          "-": lvalue - rvalue,
+          "*": lvalue * rvalue,
+          "/": lvalue / rvalue,
+          "%": lvalue % rvalue,
+        }[operator];
+      },
+    },
+  })
+);
+app.set("view engine", "handlebars");
 
 configRoutes(app);
 
 app.listen(3000, () => {
   console.log("We've now got a server!");
-  console.log('Your routes will be running on http://localhost:3000');
+  console.log("Your routes will be running on http://localhost:3000");
 });
