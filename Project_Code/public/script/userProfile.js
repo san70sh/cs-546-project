c24bc47806e97b9b4398253897779d7bc2c8a063
You Wang <w919542445@gmail.com>
2021-12-11 16:11:12 -0500

    ajax url fixed

--- Project_Code/public/script/userProfile.js
+++ Project_Code/public/script/userProfile.js
@@ -2,7 +2,7 @@ function deleteResume(id) {
   if (confirm("Are you sure you want to delete it?")) {
     delReq = {
       method: "delete",
-      url: `./resume/${id}`,
+      url: `/users/resume/${id}`,
     };
     $.ajax(delReq).then((res) => {
       alert(res.message);
